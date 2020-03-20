72fe85b5c0798aaa43f3e6b378c99c1eaeefb9f9
Luca Morricone <morricone.luca@outlook.com>
2020-03-20 16:40:39 +0100

    Add ActivityIndicator

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Provider} from 'react-redux';
 import store from './store';
 import {handleGetDecks} from './actions';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, ActivityIndicator, SafeAreaView} from 'react-native';
 
 import DeckList from './screens/DeckList';
 
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setLoading(false);
+      unsubscribe();
+    });
+
     store.dispatch(handleGetDecks());
   }, []);
 
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator size="large" color="#1c73b4" />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <Provider store={store}>
-      <DeckList />
+      <SafeAreaView style={styles.container}>
+        <DeckList />
+      </SafeAreaView>
     </Provider>
   );
 }
@@ -22,8 +39,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
